daa62820c4e6349d710bde7b691cd87670bacf7e
Wagse5 <128879233+Wagse5@users.noreply.github.com>
2024-09-29 02:35:41 -0700

    Debug postbuild.js script and update build process

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -13,12 +13,17 @@ function moveItem(source, destination) {
 const outDir = path.join(__dirname, '..', 'out');
 const publicDir = path.join(__dirname, '..', 'public');
 
+console.log('outDir:', outDir);
+console.log('publicDir:', publicDir);
+
 fs.readdirSync(outDir).forEach(file => {
+  console.log('Processing file:', file);
   if (file.endsWith('.html')) {
     moveItem(path.join(outDir, file), path.join(publicDir, file));
   }
 });
 
+console.log('Moving _next directory');
 moveItem(path.join(outDir, '_next'), path.join(publicDir, '_next'));
 
 console.log('Postbuild script completed.');
