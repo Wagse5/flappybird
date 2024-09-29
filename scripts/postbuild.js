27080fb3752c0afd4684401176e7c6568bad2edf
Wagse5 <128879233+Wagse5@users.noreply.github.com>
2024-09-29 02:30:44 -0700

    Comprehensive update to fix Vercel deployment issues

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,7 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-// Function to move a file or directory
 function moveItem(source, destination) {
   try {
     fs.moveSync(source, destination, { overwrite: true });
@@ -11,18 +10,15 @@ function moveItem(source, destination) {
   }
 }
 
-// Paths
 const outDir = path.join(__dirname, '..', 'out');
 const publicDir = path.join(__dirname, '..', 'public');
 
-// Move HTML files
 fs.readdirSync(outDir).forEach(file => {
   if (file.endsWith('.html')) {
     moveItem(path.join(outDir, file), path.join(publicDir, file));
   }
 });
 
-// Move _next directory
 moveItem(path.join(outDir, '_next'), path.join(publicDir, '_next'));
 
 console.log('Postbuild script completed.');
